f61e209604465c474f6c375cc369edd333ee043a
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2020-07-22 16:01:05 +0900

    ・関数を分割。 ・路線集約時に消失する駅が出てくる不具合を修正。

--- API/src/stations.php
+++ API/src/stations.php
@@ -3,22 +3,11 @@
 
     const TOKYO_PREF_CODE = "13";     // 東京都の都道府県コード
 
-    // データ作成
-    function make_data()
+    // 駅を列挙
+    function enumrate_stations($stations_csv, $lines_csv)
     {
-        $datas = [];
-        
-        $stations = CSVTable::open("resources/station.csv");
-        if($stations === null) { return null; }
-
-        $lines = CSVTable::open("resources/line.csv");
-        if($lines === null) { return null; }
-
-        // HACK:$datasが一時領域扱いになっている。
-        //      後程関数を分割して対処。
-
-        // 東京都の駅を列挙。
-        foreach($stations as $record)
+        $result = [];
+        foreach($stations_csv as $record)
         {
             // 都道府県コードをチェックし、東京都以外なら読み飛ばす。
             // ※当然、都外の駅は弾かれる。（ex:東京メトロ各線 和光市）
@@ -30,7 +19,7 @@
             $name = $record["station_name"];
             // 路線コード・路線名
             $line_code = $record["line_cd"];
-            $line_name = $lines->get_with_column_value("line_cd", $line_code)["line_name"];
+            $line_name = $lines_csv->get_with_column_value("line_cd", $line_code)["line_name"];
             $line = array("code" => $line_code, "name" => $line_name);
             // 経度・緯度
             $lon = $record["lon"];
@@ -42,30 +31,48 @@
                 "line" => $line,
                 "location" => $location,
             );
-            array_push($datas, $data);
+            array_push($result, $data);
         }
 
-        // 一つの駅に複数の路線が敷かれているケースを考慮した形に修正する。
+        return $result;
+    }
+
+    // 一つの駅に複数の路線が敷かれているケースを考慮した形に修正する。
+    function collect_stations($stations)
+    {
         $result = [];
-        $count = count($datas);
+        $count = count($stations);
         for($i = 0; $i < $count; $i++)
         {
-            $data = array("name" => $datas[$i]["name"]);
             $lines = [];
-            array_push($lines, $datas[$i]["line"]);
-            for($j = $count - 1; $j >= $i; $j--)
+            array_push($lines, $stations[$i]["line"]);
+            for($j = $count - 1; $j > $i; $j--)
             {
-                if($datas[$i]["name"] !== $datas[$j]["name"]) { continue; }
-
-                array_push($lines, $datas[$j]["line"]);
-                array_splice($datas, $j, 1);
+                if($stations[$i]["name"] !== $stations[$j]["name"]) { continue; }
+                
+                array_push($lines, $stations[$j]["line"]);
+                array_splice($stations, $j, 1);
             }
             array_push($result, array(
-                "name" => $datas[$i]["name"],
+                "name" => $stations[$i]["name"],
                 "lines" => $lines,
             ));
         }
 
+        return $result;
+    }
+
+    // データ作成
+    function make_data()
+    {        
+        $stations_csv = CSVTable::open("resources/station.csv");
+        if($stations_csv === null) { return null; }
+
+        $lines_csv = CSVTable::open("resources/line.csv");
+        if($lines_csv === null) { return null; }
+
+        $stations = enumrate_stations($stations_csv, $lines_csv);
+        $result = collect_stations($stations);
         return $result;
     }
 
