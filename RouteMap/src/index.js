127b28eb296a7415a6f389e5919e74023ba02c56
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2020-07-22 17:39:30 +0900

    とりあえず背景色描画。 ※CSS指定からJSでの制御に移動。

--- RouteMap/src/index.js
+++ RouteMap/src/index.js
@@ -1,3 +1,29 @@
 window.onload = function() {
-    document.getElementById("title").textContent = "東京都路線図";
+    var context = getContextFromCanvas();
+    if(context == null)
+    {
+        alert("Contextが取得できませんでした。");
+        return;
+    }
+
+    drawBackground(context);
+}
+
+// 背景色描画
+function drawBackground(context)
+{
+    context.fillStyle = 'rgb(0, 0, 255)';
+    context.globalAlpha = 0.5;
+    context.fillRect(0, 0, 1280, 700);
+}
+
+// CanvasからContextを取得。
+function getContextFromCanvas()
+{
+    var canvas = document.getElementById("drawCanvas");
+    if(canvas == null) { return null; }
+
+    if(canvas.getContext == undefined) { return null; }
+
+    return canvas.getContext("2d");
 }
