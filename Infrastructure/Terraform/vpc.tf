85aece6824be99c3b634065e2f1cee6090b56ec0
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2020-07-23 16:53:12 +0900

    warningを吐いていたので修正。

--- Infrastructure/Terraform/vpc.tf
+++ Infrastructure/Terraform/vpc.tf
@@ -9,15 +9,15 @@ resource "aws_vpc" "tokyo_metro_network_vpc" {
 
 # InternetGateway
 resource "aws_internet_gateway" "tokyo_metro_network_internet_gateway" {
-  vpc_id = "${aws_vpc.tokyo_metro_network_vpc.id}"
+  vpc_id = aws_vpc.tokyo_metro_network_vpc.id
 }
 
 # RouteTable
 resource "aws_route_table" "public" {
-  vpc_id = "${aws_vpc.tokyo_metro_network_vpc.id}"
+  vpc_id = aws_vpc.tokyo_metro_network_vpc.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_internet_gateway.tokyo_metro_network_internet_gateway.id}"
+    gateway_id = aws_internet_gateway.tokyo_metro_network_internet_gateway.id
   }
   tags = {
     Name = "public"
@@ -26,7 +26,7 @@ resource "aws_route_table" "public" {
 
 # Subnets
 resource "aws_subnet" "public_a" {
-  vpc_id = "${aws_vpc.tokyo_metro_network_vpc.id}"
+  vpc_id = aws_vpc.tokyo_metro_network_vpc.id
   cidr_block = "10.1.1.0/24"
   availability_zone = "ap-northeast-1a"
   tags = {
@@ -36,6 +36,6 @@ resource "aws_subnet" "public_a" {
 
 # SubnetRouteTableAssociation
 resource "aws_route_table_association" "public_a" {
-    subnet_id = "${aws_subnet.public_a.id}"
-    route_table_id = "${aws_route_table.public.id}"
+    subnet_id = aws_subnet.public_a.id
+    route_table_id = aws_route_table.public.id
 }
