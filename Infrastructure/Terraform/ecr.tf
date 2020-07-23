d208aa17fcb70f31bf0297827f54ec94ea1cb81b
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2020-07-23 17:06:24 +0900

    リポジトリを分割。

--- Infrastructure/Terraform/ecr.tf
+++ Infrastructure/Terraform/ecr.tf
@@ -1,5 +1,14 @@
-resource "aws_ecr_repository" "tokyo_metro_network_repos" {
-    name = "tokyo_metro_netrowk"
+resource "aws_ecr_repository" "tokyo_metro_network_api_repo" {
+    name = "yanap/tokyo_metro_netrowk_api"
+    image_tag_mutability = "MUTABLE"
+
+    image_scanning_configuration {
+        scan_on_push = true
+    }
+}
+
+resource "aws_ecr_repository" "tokyo_metro_network_route_map_repo" {
+    name = "yanap/tokyo_metro_netrowk_route-map"
     image_tag_mutability = "MUTABLE"
 
     image_scanning_configuration {
